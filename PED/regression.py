37ce048542a61dbd6a2dc1d96bad30b594ac9f45
ernestkkb <ernest.khoo.2018@smu.edu.sg>
2021-03-24 15:07:15 +0800

    Regression Analysis Completed

--- PED/regression.py
+++ PED/regression.py
@@ -4,6 +4,7 @@ import numpy as np
 import seaborn as sns
 from matplotlib.pyplot import figure
 import streamlit as st
+from PIL import Image
 
 from statsmodels.compat import lzip
 import numpy as np
@@ -12,9 +13,12 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
 
+# Image.MAX_IMAGE_PIXELS = None
+
 #Function Definition
 @st.cache(ttl = 3600)
 def read_file(filename):
+    plt.rcParams["figure.figsize"] = (12,8)
     data = pd.read_csv(filename, encoding = 'ISO-8859-1')
     return data
 
@@ -23,7 +27,7 @@ def display_regression_plot(data):
     st.pyplot()
 
 def partial_regression_plot(model):
-    fig = plt.figure(figsize=(20,14))
+    plt.rcParams["figure.figsize"] = (26,22)
     fig = sm.graphics.plot_partregress_grid(model)
     st.pyplot()
     st.subheader('Partial Regression Plot')
@@ -33,8 +37,9 @@ def partial_regression_plot(model):
     ''')
 
 def ccpr_plot(model):
-    fig = plt.figure(figsize=(12,8))
-    fig = sm.graphics.plot_ccpr_grid(model, fig = fig)
+    plt.rcParams["figure.figsize"] = (26,22)
+    ax = sm.graphics.plot_ccpr_grid(model)
+ 
     st.pyplot()
     st.subheader('Component-Component Plus Residual Plot')
     st.text('''
@@ -45,9 +50,10 @@ def ccpr_plot(model):
     ''')
 
 def reg_plot(data):
+    
     model = ols("np.log(Quantity) ~ np.log(UnitPrice)", data = data).fit()
-    fig = plt.figure(figsize=(12,8))
-    fig = sm.graphics.plot_regress_exog(model, 'np.log(UnitPrice)', fig=fig)
+    plt.rcParams["figure.figsize"] = (28,38)
+    fig = sm.graphics.plot_regress_exog(model, 'np.log(UnitPrice)')
     st.pyplot()
     st.text('''
     In graph Y and fitted vs X & Residuals vs Price, we can see that the variation between 
@@ -197,10 +203,10 @@ def app():
         rls_summary_log(data[data['StockCode'] == id])
         st.subheader('Conclusion for '+product_title)
         st.text('''Both Ordinary Least Squares (OLS) & Recursive Least Squares (RLS) are consistent 
-    with the fact that price exhibits a OPPOSITE influence on quantity. Since the price 
-    elasticity of demand is < 1, we can conclude that the item is inelastic where changes
-    in price have a less than proportional effect on the quantity of the good demanded.
-    ''')
+with the fact that price exhibits a OPPOSITE influence on quantity. Since the price 
+elasticity of demand is < 1, we can conclude that the item is inelastic where changes
+in price have a less than proportional effect on the quantity of the good demanded.
+''')
     st.subheader('Conclusion for all household items')
     st.text('''
     Household items sold on ecommerce platforms tend to be inelastic where changes in price
@@ -234,11 +240,18 @@ def app():
     taken with as much importance as we can already ascertain that the products' elasticity
     through the changes in quantity & price.''')
 
-    st.subheader('''To further improve the r squared value of the household products, 
-    we will attempt to improve its complexity via polynomial regression in a separate section.''')
+    st.text('''To further improve the r squared value of the household products, 
+we will attempt to improve its complexity via polynomial regression in a separate section.''')
 
     st.header('Regression Analysis for Electronic Products')
-    df = read_file('category_price.csv')
+    def read_file2(filename):
+        data = pd.read_csv(filename)
+        return data
+
+    df = read_file2('category_price.csv')
+    df['Date_imp'] = pd.to_datetime(df.Date_imp, format='%Y-%m-%d %H:%M:%S')
+    df['hour'] = df['Date_imp'].dt.hour
+
     df = df[df['condition'] == 'New']
     #Drop columns that are not going to be used for the project
     df = df[df.columns.drop(['condition'])]
@@ -250,37 +263,149 @@ def app():
     df_detect = df_us.groupby('name')['disc_price'].nunique()
     df_detect = df_detect.to_frame().reset_index()
     df_detect.columns = ['name','disc_price']
-    df_detect
 
     valid_items = df_detect[df_detect['disc_price'] > 5]
     valid_items = valid_items['name'].tolist()
 
     #Print Deleted Impressions with price variability
     df_clean = df_us[df_us['name'].isin(valid_items)]
+
     #Detect top selling items
-    top = df_clean.groupby('name').size().reset_index(name='counts').nlargest(15,['counts'])
-    top_products = top['name'].tolist()
+
+    top_products = ['JBL Clip2 Portable Speaker',
+                'Yamaha - Natural Sound 5 2-Way All-Weather Outdoor Speakers (Pair) - White"', 
+                'Russound - Acclaim 5 Series 6-1/2 2-Way Indoor/Outdoor Speakers (Pair) - White"',
+                'MCR-B043 30W Bluetooth Wireless Music System (Black)',
+                'Kicker DSC44 4 D-Series 2-Way Car Speakers with 1/2" Tweeters"',
+                'Alpine - 6-1/2 2-Way Component Car Speakers with Poly-Mica Cones (Pair) - Black"',
+                'Details About Alpine 400w 5.25 Typee Coaxial 2way Car Speakers | Spe5000"']
+
     df_clean = df_clean[df_clean['name'].isin(top_products)]
     frames = []
     for i in top_products:
-        test = df_clean[df_clean.name.str.contains(i)]
+        test = df_clean[df_clean['name'] == i]
         test = test[(np.abs(stats.zscore(test['disc_price'])) < 3)]
         frames.append(test)
 
     final_df = pd.concat(frames)
 
-    #Drop columns that are not going to be used for the project
-    df_clean = df_clean[['name', 'price', 'disc_price', 'merchant', 'brand', 
-            'Category_name', 'Day_n', 'year', 'month', 'month_n','day', 'Week_Number']]
+    #Check Skewness
+    def summary2(name):
+        df = final_df[final_df['name'] == name]
+        grouping = df.groupby(['disc_price']).size()
+        grouping_df = grouping.to_frame().reset_index()
+        grouping_df.columns = ['UnitPrice','Quantity']
+        return pd.DataFrame(grouping_df)
+    
+    def check_skew(top_products):
+        newDict = {
+            'Item': [],
+            'Skewness Price':[],
+            'Skewness Quantity':[]
+        }
+        for name in top_products:
+            df = summary2(name)
+            skew = df.skew()
+            newDict['Item'].append(name)
+            newDict['Skewness Price'].append(skew[0])
+            newDict['Skewness Quantity'].append(skew[1])
+            
+        return pd.DataFrame(newDict)
 
-    testing = final_df.loc[final_df['name'] == 'JBL Clip2 Portable Speaker']
-    tester = testing.groupby(['disc_price']).size()
-    test_df = tester.to_frame().reset_index()
-    test_df.columns = ['UnitPrice','Quantity']
-    st.subheader('Regression Plot')
-    display_regression_plot(test_df)
-    st.subheader('Ordinary Least Squares (OLS) Estimation')
-    ols_summary_log1(test_df)
-    st.subheader('Rercursive Least Squares (RLS) Estimation')
-    rls_summary_log(test_df)
-    st.dataframe(PED1(test_df))
+    top_products_skew = check_skew(top_products)
+    st.dataframe(top_products_skew)
+
+    def all_top_regress(i):
+        testing = final_df.loc[final_df['name'] == i]
+        tester = testing.groupby(['disc_price']).size()
+        test_df = tester.to_frame().reset_index()
+        test_df.columns = ['UnitPrice','Quantity']
+        return display_regression_plot(test_df)
+
+    for i in top_products:
+        st.subheader(i + " Regression Plot")
+        all_top_regress(i)
+
+    def ols_summary_log_skewQ(data):
+        model = ols("np.log(Quantity) ~ UnitPrice", data = data).fit()
+        st.text(model.summary())
+
+    def ols_summary_log_skewP(data):
+        model = ols("Quantity ~ np.log(UnitPrice)", data = data).fit()
+        st.text(model.summary())
+
+    def ols_summary_log_skewBoth(data):
+        model = ols("np.log(Quantity) ~ np.log(UnitPrice)", data = data).fit()
+        st.text(model.summary())    
+
+    def all_top(i):
+        testing = final_df.loc[final_df['name'] == i]
+        tester = testing.groupby(['disc_price']).size()
+        test_df = tester.to_frame().reset_index()
+        test_df.columns = ['UnitPrice','Quantity']
+        
+        skew = []
+        skew_values = test_df.skew()
+        skew.append(skew_values[0])
+        skew.append(skew_values[1])
+        
+        if abs(skew[0]) > 0.5 and abs(skew[1]) > 0.5:
+            return ols_summary_log_skewBoth(test_df)
+        
+        elif abs(skew[0]) < 0.5 and abs(skew[1]) > 0.5:
+            return ols_summary_log_skewQ(test_df)
+        
+        elif abs(skew[0]) > 0.5 and abs(skew[1]) < 0.5:
+            return ols_summary_log_skewP(test_df)
+
+    for i in top_products:
+        print('*'*20,i, "*"*20)
+        st.subheader(i + " OLS Summary Log")
+        all_top(i)
+
+    st.text('''
+Based on the P values above, we are unable to reject the null hypothesis of 2 items: Yamaha
+- Natural Sound 5 2-Way All-Weather Outdoor Speakers (Pair) - White'' and  Details About 
+Alpine 400w 5.25 Typee Coaxial 2way Car Speakers | Spe5000" as they  have a P value of 0.052
+and 0.085, above the significance level. Hence, there might not be a real relationship 
+between price and quantity even if the r^2 is high as it might be due to pure chance. 
+However, for the rest of the items, we can see that P Value is below the significance level 
+of 0.05 and can reject the null hypothesis. R^2 values for all the 7 products are at least 
+about 0.5 which represents that the  model is able to explain the variation in quantity 
+demanded at least half the time. This models will then be used for PED calculations.
+''')
+
+    def PED2(top_products): 
+        newDict = {
+            'Item': [],
+            'Price Elasticity of Demand':[]
+        }
+        
+        for name in top_products: 
+            data = summary2(name)
+            model = ols("np.log(Quantity) ~ UnitPrice", data = data).fit()
+            intercept, slope = model.params
+            mean_price = np.mean(data['UnitPrice'])
+            mean_quantity = np.mean(data['Quantity'])
+            price_elasticity = ((slope) * (mean_price/mean_quantity))
+
+            newDict['Item'].append(name)
+            newDict['Price Elasticity of Demand'].append(price_elasticity)
+        
+        return pd.DataFrame(newDict)
+    st.dataframe(PED2(top_products))
+
+    st.text('''
+We can see that there is quite a distribution of elasticities. Also, it is worthy to note
+that most of these Speaker products are considered Veblen goods (due to the positive PED)
+except for one. This means that as price increases, quantity demanded increases too. 
+Interpreting each item PED values are as follows:
+
+1) JBL Clip2 Portable speaker: PED of 0.461 indicates that a 10% increase in the price for the jar results in a 4.61% increase in quantity demanded. Absolute value of PED is less than 1 so its quantity demanded is price inelastic.
+2) Yamaha - Natural Sound 5 2-Way All-Weather Outdoor Speakers (Pair) - White: PED of 1.205 indicates that a 10% increase in the price for the bag results in a 12% increase in quantity demanded. Absolute value of PED is more than 1 so its quantity demanded is price elastic.
+3) Russound - Acclaim 5 Series 6-1/2 2-Way Indoor/Outdoor Speakers (Pair) - White": PED of 0.527 indicates that a 10% increase in the price for the light holder results in a 5.27% increase in quantity demanded. Absolute value of PED is less than 1 so its quantity demanded is price inelastic.
+4) MCR-B043 30W Bluetooth Wireless Music System (Black): PED of 2.58 indicates that a 10% increase in the price for the light results in a 25% increase in quantity demanded. Absolute value of PED is more than 1 so its quantity demanded is price elastic.
+5) Kicker DSC44 4 D-Series 2-Way Car Speakers with 1/2" Tweeters": PED of -1.619 indicates that a 10% increase in the price for the popcorn holder results in a 16% decrease in quantity demanded. Absolute value of PED is more than 1 so its quantity demanded is price elastic.
+6) Alpine - 6-1/2 2-Way Component Car Speakers with Poly-Mica Cones (Pair) - Black": PED of 0.675 indicates that a 10% increase in the price for the light holder results in a 6.75% increase in quantity demanded. Absolute value of PED is less than 1 so its quantity demanded is price inelastic.
+7) Details About Alpine 400w 5.25 Typee Coaxial 2way Car Speakers | Spe5000": PED of 0.735 indicates that a 10% increase in the price for the light holder results in a 7.35% increase in quantity demanded. Absolute value of PED is less than 1 so its quantity demanded is price inelastic.
+    ''')
